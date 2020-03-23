badc31dc6e8f78c6c183ce3a8728b6269ac39f4f
Mohammedsamib <mohaa.sa199@gmail.com>
2020-03-23 17:02:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace The_first_one
+{
+    class Program
+    {
+        static int Toplam()
+        {
+            int x = 6, y = 6;
+            return x + y;
+        }
+      
+        static void Main(string[] args)
+        {
+            calisan c1 = new calisan() ;
+
+            c1.Code = "XCWEWE";
+            c1.name = "Mohammed";
+            c1.telefon = "05524677219";
+            c1.salary = 7500;
+            c1.printdata();
+            calisan c2 = new calisan();
+
+            c2.Code = "RTRRT";
+            c2.printdata();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
